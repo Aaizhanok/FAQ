9bb964c999246cc81b1f8cbe39c91d6c3ca611f1
Aaizhanok <88786657+Aaizhanok@users.noreply.github.com>
2021-09-24 18:04:36 +0600

    add

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+export default class Button{
+    render() {
+        return `
+            <button class="quiz-btn">
+                    <span class="plus-icon" id="plusBtn"><i class="fa fa-plus-circle" aria-hidden="true"></i></span>
+                    <span class="minus-icon" id="minusBtn"><i class="fa fa-minus-circle" aria-hidden="true"></i></span>
+            </button>
+            `
+    }
+}
